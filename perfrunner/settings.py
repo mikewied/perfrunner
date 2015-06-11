cafcfaa27b51c66d2ea5edba95da43e599274704
Mike Wiederhold <mikewied06@gmail.com>
2015-06-11 11:28:44 -0700

    Update requirements.txt
    Change-Id: Ic5be6bfccadbf1b57014efc8c0443cdfba79c18c

--- perfrunner/settings.py
+++ perfrunner/settings.py
@@ -9,7 +9,7 @@ from logger import logger
 from perfrunner.helpers.misc import uhex
 
 
-REPO = 'https://github.com/couchbase/perfrunner'
+REPO = 'https://github.com/mikewied/perfrunner'
 
 
 @decorator
